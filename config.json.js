841958d61684ef1a03a70ec7b6d114e4cc597766
mritd <mritd1234@gmail.com>
2017-05-16 23:29:14 +0800

    完善 README

--- config.json.js
+++ config.json.js
@@ -2,7 +2,7 @@
     "build_date": "2017-05-03_19:15:58", 
     "description": "Godaddy DDNS", 
     "home_url": "Module_gdddns.asp", 
-    "md5": "493cc4ca3f2a6487f20f24e075550c57", 
+    "md5": "ee8d29d2a714806dee4ae0fa8b825f22", 
     "module": "gdddns", 
     "title": "Godaddy DDNS", 
     "version": "1.0.0"
